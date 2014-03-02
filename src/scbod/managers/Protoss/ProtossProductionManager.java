8ddda42f8dac62ff55e58d4b75029782ac0f73d2
AKAfreaky <frogman.jo@gmail.com>
2014-03-02 16:56:10 +0000

    Add a training queue
    Looks like the same issue as with workers

--- src/scbod/managers/Protoss/ProtossProductionManager.java
+++ src/scbod/managers/Protoss/ProtossProductionManager.java
@@ -1,5 +1,8 @@
 package scbod.managers.Protoss;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 import jnibwapi.JNIBWAPI;
 import jnibwapi.model.Unit;
 import jnibwapi.types.UnitType.UnitTypes;
@@ -10,6 +13,8 @@ import scbod.managers.ResourceManager;
 public class ProtossProductionManager extends ProductionManager
 {
 	private ProtossBuildingManager buildingManager;
+	private ArrayList<Point> buildQueue = new ArrayList<Point>();
+	
 	
 	public ProtossProductionManager(JNIBWAPI bwapi, ResourceManager resourceManager, BuildingManager buildingManager)
 	{
@@ -23,6 +28,17 @@ public class ProtossProductionManager extends ProductionManager
 	{
 		int incomingSupply = buildingManager.getUnfinishedPylonCount() * 8; 
 		resourceManager.setPredictedSupplyTotal(resourceManager.getSupplyTotal() + incomingSupply);
+		
+		if(buildQueue.size() > 0)
+		{
+			for(Point point: buildQueue)
+			{
+				bwapi.train(point.x, point.y);
+			}
+			
+			buildQueue.clear();
+		}
+		
 	}
 	
 	
@@ -36,6 +52,7 @@ public class ProtossProductionManager extends ProductionManager
 			{
 				if( resourceManager.getMineralCount() >= 50 && resourceManager.getSupplyAvailable() >= 1)
 				{
+					buildQueue.add(new Point(unit.getID(), UnitTypes.Protoss_Probe.ordinal()));
 					bwapi.train(unit.getID(), UnitTypes.Protoss_Probe.ordinal());
 					return true;
 				}
@@ -65,14 +82,19 @@ public class ProtossProductionManager extends ProductionManager
 		{
 			if( resourceManager.getMineralCount() >= 100 && resourceManager.getSupplyAvailable() >= 1)
 			{
-				bwapi.train(gateway.getID(), UnitTypes.Protoss_Zealot.ordinal());
-				return true;
+				buildQueue.add(new Point(gateway.getID(), UnitTypes.Protoss_Zealot.ordinal()));
+				return bwapi.train(gateway.getID(), UnitTypes.Protoss_Zealot.ordinal());
 			}
 			else
 			{
 				System.out.println("Don't have the supply or minerals to build a zealot");
 			}
 		}
+		else
+		{
+			System.out.println("No Gateway?");
+		}
+		
 		
 		return false;
 	}
