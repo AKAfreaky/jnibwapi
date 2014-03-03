5b7fb8d4de98cb8e41ba8471daa045131db8c5e4
Alex <frogman.jo@gmail.com>
2014-03-03 12:05:28 +0000

    Train medics

--- src/scbod/managers/Terran/TerranProductionManager.java
+++ src/scbod/managers/Terran/TerranProductionManager.java
@@ -3,6 +3,7 @@ package scbod.managers.Terran;
 import scbod.managers.BuildingManager;
 import scbod.managers.ProductionManager;
 import scbod.managers.ResourceManager;
+import scbod.managers.UnitManager;
 import jnibwapi.JNIBWAPI;
 import jnibwapi.model.Unit;
 import jnibwapi.types.UnitType.UnitTypes;
@@ -10,6 +11,7 @@ import jnibwapi.types.UnitType.UnitTypes;
 public class TerranProductionManager extends ProductionManager {
 
 	private TerranBuildingManager buildingManager;
+	private UnitManager unitManager;
 	
 	public TerranProductionManager(JNIBWAPI bwapi, ResourceManager resourceManager, BuildingManager buildingManager)
 	{
@@ -33,7 +35,7 @@ public class TerranProductionManager extends ProductionManager {
 		{
 			if(unit.getTypeID() == UnitTypes.Terran_Command_Center.ordinal())
 			{
-				if( resourceManager.getMineralCount() >= 50 && resourceManager.getSupplyAvailable() >= 1)
+				if( resourceManager.getMineralCount() >= 50 && resourceManager.getSupplyAvailable() >= 2)
 				{
 					bwapi.train(unit.getID(), UnitTypes.Terran_SCV.ordinal());
 					return true;
@@ -65,22 +67,53 @@ public class TerranProductionManager extends ProductionManager {
 		if (rax != null)
 		{
 			raxExists = true;
-			if( resourceManager.getMineralCount() >= 50 && resourceManager.getSupplyAvailable() >= 1)
+			if( resourceManager.getMineralCount() >= 50 && resourceManager.getSupplyAvailable() >= 2)
 			{
 				bwapi.train(rax.getID(), UnitTypes.Terran_Marine.ordinal());
 				return true;
 			}
 			else
 			{
-				System.out.println("Don't have the supply or minerals to build an SCV");
+				System.out.println("Don't have the supply or minerals to build a Marine");
 			}
 		}
 		
 		if (!raxExists)
 		{
-			System.out.println("No command center found (!!!)");
+			System.out.println("No Barracks found");
 		}
 		
+		
+		return false;
+	}
+	
+	public boolean trainMedic()
+	{		
+		if(unitManager.getMyUnitOfType(UnitTypes.Terran_Academy.ordinal(), true) != null)
+		{
+			Unit rax = buildingManager.getLeastBusyBarracks();
+			
+			if (rax != null)
+			{
+				if( resourceManager.getMineralCount() >= 50 && resourceManager.getSupplyAvailable() >= 2)
+				{
+					bwapi.train(rax.getID(), UnitTypes.Terran_Medic.ordinal());
+					return true;
+				}
+				else
+				{
+					System.out.println("Don't have the supply or minerals to build a Medic");
+				}
+			}
+			else
+			{
+				System.out.println("No Barracks found");
+			}
+		}
+		else
+		{
+			System.out.println("Need an Academy to train Medics!");
+		}
 		
 		return false;
 	}
