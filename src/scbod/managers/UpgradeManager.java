85b4df1dedc80f06019fbc27eab70362fffb51e1
AKAfreaky <frogman.jo@gmail.com>
2014-03-10 00:00:12 +0000

    Initial generic upgrades
    Needs work to account for the tech/upgrade split (which seems totally
    arbitary at first glance)

--- src/scbod/managers/UpgradeManager.java
+++ src/scbod/managers/UpgradeManager.java
@@ -1,7 +1,50 @@
 package scbod.managers;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
+import jnibwapi.JNIBWAPI;
+import jnibwapi.model.Unit;
+import jnibwapi.types.UpgradeType;
+
 
 public class UpgradeManager extends Manager
 {
-
+	protected JNIBWAPI bwapi;
+	protected UnitManager unitManager;
+	
+	protected ArrayList<Point> upgradeQueue = new ArrayList<Point>();
+	
+	public boolean upgradeTech( UpgradeType.UpgradeTypes upgradeType )
+	{
+		int upgradeTypeID = upgradeType.ordinal();
+		
+		if(bwapi.canResearch(upgradeTypeID))
+		{
+			int researcherType = bwapi.getUpgradeType(upgradeTypeID).getWhatUpgradesTypeID();
+			Unit researcher = unitManager.getLeastBusyUnitofType(researcherType);
+			
+			if(bwapi.canResearch(researcher.getID(), upgradeTypeID))
+			{
+				upgradeQueue.add(new Point(researcher.getID(), upgradeTypeID));
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public void gameUpdate()
+	{
+		if(upgradeQueue.size() > 0)
+		{
+			for(Point point: upgradeQueue)
+			{
+				bwapi.upgrade(point.x, point.y);
+			}
+			
+			upgradeQueue.clear();
+		}
+	}
+	
 }
