6c7f5080611318c5a054c9994cea1536326de2a2
AKAfreaky <frogman.jo@gmail.com>
2014-03-13 00:27:37 +0000

    Control dragoons and observers as protoss

--- src/scbod/managers/Protoss/ProtossMilitaryManager.java
+++ src/scbod/managers/Protoss/ProtossMilitaryManager.java
@@ -20,18 +20,35 @@ public class ProtossMilitaryManager extends MilitaryManager
 		super(bwapi, intelligenceManager, unitManager, workerManager);
 		// Initialise these so they aren't null
 		unitGroups.put(UnitTypes.Protoss_Zealot.ordinal(), new HashSet<Integer>());
+		unitGroups.put(UnitTypes.Protoss_Dragoon.ordinal(), new HashSet<Integer>());
+		unitGroups.put(UnitTypes.Protoss_Observer.ordinal(), new HashSet<Integer>());
 	}
 	
 	/** Moves all units to the current destination */
 	@Override
 	protected void moveUnits()
 	{
+		super.moveUnits();
 		int i = 0;
 		
 		HashSet<Integer> zealots = zealots();
+		HashSet<Integer> dragoons = dragoons();
+		HashSet<Integer> observers = observers();
 		
-		int total = zealots.size();
+		int total = zealots.size() + dragoons.size();
 		for (int unitID : zealots)
+		{
+			moveToDestination(unitID, i, total);
+			i++;
+		}
+		
+		for (int unitID : dragoons)
+		{
+			moveToDestination(unitID, i, total);
+			i++;
+		}
+		
+		for (int unitID : observers)
 		{
 			moveToDestination(unitID, i, total);
 			i++;
@@ -45,8 +62,20 @@ public class ProtossMilitaryManager extends MilitaryManager
 		super.attackUnits();
 		
 		HashSet<Integer> zealots = zealots();
+		HashSet<Integer> dragoons = dragoons();
+		HashSet<Integer> observers = observers();
 		
 		for (int unitID : zealots)
+		{
+			sendToAttackBasic(unitID);
+		}
+		
+		for (int unitID : dragoons)
+		{
+			sendToAttackBasic(unitID);
+		}
+		
+		for (int unitID : observers)
 		{
 			sendToAttackBasic(unitID);
 		}
@@ -55,6 +84,26 @@ public class ProtossMilitaryManager extends MilitaryManager
 	private HashSet<Integer> zealots()
 	{
 		HashSet<Integer> retVal = unitGroups.get(UnitTypes.Protoss_Zealot.ordinal());
+		if (retVal == null)
+		{
+			retVal = new HashSet<Integer>();
+		}
+		return retVal;
+	}
+	
+	private HashSet<Integer> dragoons()
+	{
+		HashSet<Integer> retVal = unitGroups.get(UnitTypes.Protoss_Dragoon.ordinal());
+		if (retVal == null)
+		{
+			retVal = new HashSet<Integer>();
+		}
+		return retVal;
+	}
+	
+	private HashSet<Integer> observers()
+	{
+		HashSet<Integer> retVal = unitGroups.get(UnitTypes.Protoss_Observer.ordinal());
 		if (retVal == null)
 		{
 			retVal = new HashSet<Integer>();
